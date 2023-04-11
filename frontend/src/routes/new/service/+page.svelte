08b4191b2ecba3d440514dc3e149abd84ebaff98
Your Name <you@example.com>
2023-04-11 14:26:07 +0000

    make getServiceList a lot faster, also add more ops

--- frontend/src/routes/new/service/+page.svelte
+++ frontend/src/routes/new/service/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import GreyText from "$lib/components/GreyText.svelte";
 	import InputSm from "$lib/components/InputSm.svelte";
+	import Select from "$lib/components/Select.svelte";
 
     let name: string;
+    let command: string = "/usr/bin/";
+    let directory: string = "/root/";
+    let target: string;
+    let description: string;
+    let after: string = "ibl-maint"; // Usually what you want
+    let brokenValue: string = "0";
 </script>
 
 <h1 class="text-2xl font-semibold">Create New Service</h1>
@@ -16,5 +23,49 @@
         placeholder="arcadia, ibl-backup etc."
         bind:value={name}
         minlength={1}
+    />
+    <InputSm 
+        id="command"
+        label="Command (must start with /usr/bin/)"
+        placeholder="E.g. /usr/bin/arcadia"
+        bind:value={command}
+        minlength={3}
+    />
+    <InputSm 
+        id="directory"
+        label="Directory"
+        placeholder="E.g. /root/arcadia"
+        bind:value={directory}
+        minlength={3}
+    />
+    <InputSm 
+        id="target"
+        label="Target"
+        placeholder="E.g. ibl"
+        bind:value={target}
+        minlength={1}
+    />
+    <InputSm
+        id="description"
+        label="Description"
+        placeholder="E.g. Arcadia"
+        bind:value={description}
+        minlength={5}
+    />
+    <InputSm
+        id="after"
+        label="After"
+        placeholder="E.g. ibl-maint"
+        bind:value={after}
+        minlength={1}
+    />
+    <Select
+        name="broken"
+        placeholder="Is the service broken/disabled?"
+        bind:value={brokenValue}
+        options={new Map([
+            ["Yes, it is", "0"],
+            ["No, its not", "1"],
+        ])}
     />
 </div>
