49b740e773b56619543b849a23c28b6e6dae71c8
sysmanage-web[auto] <>
2023-04-23 14:31:00 +0000

    ci(update): persist changes to git

--- frontend/src/lib/components/LinkCard.svelte
+++ frontend/src/lib/components/LinkCard.svelte
@@ -9,7 +9,7 @@
     export let showButton: boolean = true;
 </script>
 
-<div class="w-96 h-[26rem] p-6 m-3 bg-white shadow-md rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+<div class="w-full block p-6 m-3 md:w-1/2 sm:w-96 bg-white rounded-lg shadow-md border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
     <h5 class="mb-2 text-2xl font-bold text-gray-900 dark:text-white">{title}</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 h-72"><slot /></p>
     
