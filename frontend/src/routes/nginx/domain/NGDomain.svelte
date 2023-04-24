3509bc2711e25b48da4eac408aad284b3eba2352
sysmanage-web[auto] <>
2023-04-24 17:08:07 +0000

    ci(update): persist changes to git

---
+++ frontend/src/routes/nginx/domain/NGDomain.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import NgServer from "./NGServer.svelte";
+    import ObjectRender from "$lib/components/ObjectRender.svelte";
+
+    export let domain: any;
+</script>
+
+<h1 class="text-2xl font-semibold">Viewing {domain?.Domain}</h1>
+
+<h2 class="text-xl font-semibold">Server List</h2>
+
+<div class="flex flex-col space-y-2">
+    {#each domain?.Server?.Servers as server, i}
+        <NgServer bind:server={server} i={i} />
+    {/each}
+</div>
+<small>
+    <ObjectRender object={domain} />
+</small>
