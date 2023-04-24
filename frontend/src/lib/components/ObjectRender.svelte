8fca35832435e481d43376a439b5fb5d8e254289
sysmanage-web[auto] <>
2023-04-24 16:36:51 +0000

    ci(update): persist changes to git

--- frontend/src/lib/components/ObjectRender.svelte
+++ frontend/src/lib/components/ObjectRender.svelte
@@ -2,15 +2,19 @@
     export let object: any;
 </script>
 
-{#each Object.entries(object) as [key, value]}
-    {#if typeof value === "object" && value !== null}
-        <span class="font-semibold">{key}:</span>
-        <div class="ml-4">
-            <svelte:self object={value} />
-        </div>
-    {:else}
-        <p>
-            <span class="font-semibold">{key}:</span> {value}
-        </p>
-    {/if}
-{/each}
+{#if object}
+    {#each Object.entries(object) as [key, value]}
+        {#if typeof value === "object" && value !== null}
+            <span class="font-semibold">{key}:</span>
+            <div class="ml-4">
+                <svelte:self object={value} />
+            </div>
+        {:else}
+            <p>
+                <span class="font-semibold">{key}:</span> {value}
+            </p>
+        {/if}
+    {/each}
+{:else}
+    <p class="text-red-500">No information can be provided about this object</p>
+{/if}
