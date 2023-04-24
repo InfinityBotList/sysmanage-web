a385fb38d0e2648cf69a6fd95a08809ec1bbd68d
sysmanage-web[auto] <>
2023-04-24 14:55:20 +0000

    ci(update): persist changes to git

--- frontend/src/lib/components/Section.svelte
+++ frontend/src/lib/components/Section.svelte
@@ -6,22 +6,7 @@
 
 <details open={open} class="hover:cursor-pointer">
     <summary>
-        <h4 class="font-semibold text-xl opacity-80">{title}</h4>
-
-        <div class="flex flex-row-reverse">
-            <svg
-                class="w-6 h-6 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 9l-7 7-7-7" />
-            </svg>
-        </div>
+        <span class="font-semibold text-xl opacity-80">{title}</span>
     </summary>
 
     <slot />
