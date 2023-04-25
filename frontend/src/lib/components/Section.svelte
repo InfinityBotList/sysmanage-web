b35e2f33ba9a4cfd58aea5a72e7df93a3dd2ae72
sysmanage-web[auto] <>
2023-04-25 17:46:16 +0000

    ci(update): persist changes to git

--- frontend/src/lib/components/Section.svelte
+++ frontend/src/lib/components/Section.svelte
@@ -4,7 +4,7 @@
     export let open: boolean = false;
 </script>
 
-<details open={open} class="p-5">
+<details open={open} class="ml-5 mr-5">
     <summary class="font-semibold text-xl opacity-80 hover:cursor-pointer align-middle">
         <span>{title}</span>
     </summary>
