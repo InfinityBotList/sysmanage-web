989bbf3c1139b83a34fc1b8d51a528b5b84e19b5
sysmanage-web[auto] <>
2023-04-25 19:22:34 +0000

    ci(update): persist changes to git

--- frontend/src/lib/components/InputSm.svelte
+++ frontend/src/lib/components/InputSm.svelte
@@ -5,6 +5,7 @@
     export let minlength: number;
     export let value: string = "";
     export let showErrors: boolean = true;
+    export let description: string = "";
 
     let success: boolean | null = null;
 
@@ -30,7 +31,9 @@
 
 <div class="mb-6">
     <label for={id} class="block mb-1 font-medium text-gray-900 dark:text-gray-300">{label}</label>
-    <span class="text-md text-gray-500 dark:text-gray-400 mb-2"><slot /></span>
+    {#if description}
+        <span class="text-md text-gray-500 dark:text-gray-400 mb-2">{description}</span>
+    {/if}
     <input on:change={checkLength} minlength={minlength} type="text" id={id} class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required bind:value />
 
     {#if success == true}
