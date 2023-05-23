12ef7722763efd5f0ee33bdf4134e33c35b7d7d6
Your Name <you@example.com>
2023-05-23 06:50:14 +0000

    add actions frontend (WIP)

--- plugins/actions/init.go
+++ plugins/actions/init.go
@@ -1,8 +1,19 @@
 package actions
 
-import "github.com/infinitybotlist/sysmanage-web/types"
+import (
+	"github.com/infinitybotlist/sysmanage-web/plugins/frontend"
+	"github.com/infinitybotlist/sysmanage-web/types"
+)
 
 func InitPlugin(c *types.PluginConfig) error {
+	// Register links
+	frontend.AddLink(c, frontend.Link{
+		Title:       "Custom Actions",
+		Description: "Execute custom actions that are defined by plugins.",
+		LinkText:    "View Nginx Config",
+		Href:        "@root",
+	})
+
 	loadActionsApi(c.Mux)
 	return nil
 }
