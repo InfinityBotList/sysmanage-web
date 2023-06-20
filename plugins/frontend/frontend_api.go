c112d0dffff8ac21cb588de37de3ae62e5a2b0d2
Your Name <you@example.com>
2023-06-20 11:47:31 +0000

    f

--- plugins/frontend/frontend_api.go
+++ plugins/frontend/frontend_api.go
@@ -2,62 +2,72 @@ package frontend
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/infinitybotlist/sysmanage-web/plugins/acl"
 )
 
-func loadFrontendApi(r chi.Router) {
-	r.Post("/getRegisteredLinks", func(w http.ResponseWriter, r *http.Request) {
-		var reg []Link
+// Gets the registered links on the site taking ACLS etc into account
+func GetRegisteredLinks(r *http.Request) ([]Link, error) {
+	var reg []Link
 
-		if acl.Enabled() {
-			var aclResultCache = make(map[string]bool)
+	if acl.Enabled() {
+		var aclResultCache = make(map[string]bool)
 
-			for _, link := range RegisteredLinks {
-				// Cached
-				if status, ok := aclResultCache[link.Plugin]; ok {
-					if status {
-						// Cached as true, append
-						reg = append(reg, link)
-					}
-
-					// Cached as false, continue
-					continue
-				}
-
-				userId := r.Header.Get("X-DP-UserID")
-
-				if userId == "" {
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte("SAFETY VIOLATION: user id is unset"))
-					return
-				}
-
-				req, err := http.NewRequest("GET", "/api/"+link.Plugin+"/@frontend", nil)
-
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte("Failed to check acls for " + link.Plugin))
-					return
-				}
-
-				chk := acl.CheckACL(userId, req)
-
-				if chk == nil {
-					// Allowed
+		for _, link := range RegisteredLinks {
+			// Cached
+			if status, ok := aclResultCache[link.Plugin]; ok {
+				if status {
+					// Cached as true, append
 					reg = append(reg, link)
-					aclResultCache[link.Plugin] = true
-				} else {
-					// Disallowed
-					if len(chk.PerPlugin) > 0 || len(chk.PerRoute) > 0 {
-						aclResultCache[link.Plugin] = false
-					}
+				}
+
+				// Cached as false, continue
+				continue
+			}
+
+			userId := r.Header.Get("X-DP-UserID")
+
+			if userId == "" {
+				return nil, errors.New("user id is unset")
+			}
+
+			req, err := http.NewRequest("GET", "/api/"+link.Plugin+"/@frontend", nil)
+
+			if err != nil {
+				return nil, errors.New("failed to check acls for " + link.Plugin)
+			}
+
+			chk := acl.CheckACL(userId, req)
+
+			if chk == nil {
+				// Allowed
+				reg = append(reg, link)
+				aclResultCache[link.Plugin] = true
+			} else {
+				// Disallowed
+				if len(chk.PerPlugin) > 0 || len(chk.PerRoute) > 0 {
+					aclResultCache[link.Plugin] = false
 				}
 			}
-		} else {
-			reg = RegisteredLinks
+		}
+	} else {
+		reg = RegisteredLinks
+	}
+
+	return reg, nil
+}
+
+func loadFrontendApi(r chi.Router) {
+	r.Post("/getRegisteredLinks", func(w http.ResponseWriter, r *http.Request) {
+		reg, err := GetRegisteredLinks(r)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("SAFETY VIOLATION: " + err.Error()))
+			return
 		}
 
 		bytes, err := json.Marshal(reg)
